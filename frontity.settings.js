579058f1ba06c6c95bc4da50b4765458fa0978f3
afeef-techwyse <afeef.techintl@gmail.com>
2025-07-09 16:05:31 +0530

    update

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 // const wpSiteUrl = 'https://codesign82.com/oppono';
 // const wpSiteUrl = 'https://master-7rqtwti-oer23r2mz66wc.ca-1.platformsh.site';
-const wpSiteUrl = 'https://oppono-app.com';
-const frontEndSiteUrl = 'https://oppono-app-staging.vercel.app';
+const wpSiteUrl = 'https://staging-5em2ouy-oer23r2mz66wc.ca-1.platformsh.site';
+const frontEndSiteUrl = 'https://oppono-staging-git-main-oasis-techwyses-projects.vercel.app';
 // const frontEndSiteUrl = 'http://localhost:3000';
 
 
