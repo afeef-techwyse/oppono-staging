b6c7e5f695b2b20b51c95277e10ba8057aec5743
afeef-techwyse <afeef.techintl@gmail.com>
2025-07-25 17:48:28 +0530

    update

--- build/api/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-// Import your Frontity production bundle:
-const server = require("../build/server").default;
-
-module.exports = async (req, res) => {
-  const httpServer = http.createServer(server);
-  httpServer.emit("request", req, res);
-};
